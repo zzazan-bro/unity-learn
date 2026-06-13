9c4dcdfea1e09ec0f75b69e2c42927f50540584d
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 15:06:55 +0900

    오후3시06분 정수+실수 예제

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -4,18 +4,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
 
     int a = 100;
-    long b = 100;
+    float b = 100.15f;
     
-    long sum;
+    float sum;
 
    //인트 선언 가정 int sum;
-   
-        void Start()
-    {sum = a + b ; // -> 오류안뜨고 200뜸
 
-//but 위에 가 int sum; 이렇게 long이 아니라 int로 선언하면 오류뜸 
-//왜냐하면, a는 int형이고 b는 long형이기 때문입니다, (long>int)기때문에 인트 변수로 롱의 값을 담을 수 없음
-  
+        void Start()
+    {sum = a + b ; 
+//float 선언시에는 오류 발생안하고 220.15로 출력됨. int 선언시에는 오류 발생함.
+//왜냐하면 int(정수)는 소수점을 표현할 수 없기 때문에 float(소수점까지 나타내는 실수)로 선언된b를 표현불가
+// 강제로 int로변환하는법 있음 묶고나서 인트로선언 캐스트라고함
+
+sum= (int)(a+b);
+//이렇게하면 소수점이 버려지고 220으로 출력됨.
+
+
     }
 
     // Update is called once per frame
