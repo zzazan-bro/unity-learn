95169f2356113b407f0dc2cab1293fcd28bc4180
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 14:12:32 +0900

    오후2시12분(2)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     int intValue;
-
     float FloatValue = 10.5f;
-
+그런데 지금 수정한거랑 이전꺼도 같이 올라가있어야하는거아냐? 이전꺼는 안올라가있는데
     void FloatToint()
     {
         intValue=(int)FloatValue;
@@ -19,7 +18,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         FloatToint();
 
         print(intValue);
-        
+
     }
 
     // Update is called once per frame
