eeb42a7386f8b53d2b0efcb788473d3528c657e4
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 15:00:40 +0900

    6.13오후3시(int+long예제)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,29 +3,18 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-    long a = 40000000;
-
-    // sbyte 음수 정수 자료형 (-128~127) -1바이트
-    // byte 정수 자료형 (0~255) -1바이트
-    // short 정수 자료형 (-3만~+3만) -2바이트
-    // integer 정수 자료형 (-20억~+20억) -4바이트)
-    // long 정수 자료형 (그냥무한) -8바이트)
-
-    float f = 4.000001f;
-    double d = 4.000001;
-    decimal m = 4.000001m;
-
-    // float 실수 자료형 (7자리까지 표현) -4바이트
-    // double 실수 자료형 (15자리까지 표현) -8바이트
-    // decimal 실수 자료형 (28~29자리까지 표현) -16바이트
-
-    string s = "asdfasdf";
-    char c = 'A'; //-> 글자를 유니코드로인식 사실 0093 같은 값이들어감
+    int a = 100;
+    long b = 100;
     
-    //문자 자료형
+    long sum;
 
+   //인트 선언 가정 int sum;
+   
         void Start()
-    {
+    {sum = a + b ; // -> 오류안뜨고 200뜸
+
+//but 위에 가 int sum; 이렇게 long이 아니라 int로 선언하면 오류뜸 
+//왜냐하면, a는 int형이고 b는 long형이기 때문입니다, (long>int)기때문에 인트 변수로 롱의 값을 담을 수 없음
   
     }
 
