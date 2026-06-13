8cedfae4baef75b38e4bc869b880f9a66401fc12
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 14:36:13 +0900

    6.13오후2시35분(변수1강복습)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,22 +2,29 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    int intValue;
-    float FloatValue = 10.5f;
-그런데 지금 수정한거랑 이전꺼도 같이 올라가있어야하는거아냐? 이전꺼는 안올라가있는데
-    void FloatToint()
-    {
-        intValue=(int)FloatValue;
-ㅇㅇ
 
-    }
+    // 1+1=2
+    // 숫자 1 - 상수
+
+    // 1+x=랜덤
+    // 숫자 1 - 상수
+    // x - 변수
+    int x = 100; 
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        FloatToint();
+     print(x);
 
-        print(intValue);
+    x= -500;
+     print(x);
+
+    x = x-500;
+     print(x);
+
+        x = x+1000;
+        print(x);
 
     }
 
