5d07ed7ac09759e3047ab72e179a28b23b75264e
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 13:08:27 +0900

    코딩공부 6월13일

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    bool aaaa = true;
+    bool bbbb = false;
+    
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
