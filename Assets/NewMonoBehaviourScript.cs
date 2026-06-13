fa8ea99e1b64caaf1dc3d3c18d6f2dece8aceb9f
zzazan-bro <qkfrks7@gmail.com>
2026-06-13 15:25:02 +0900

    6/13오후3시24분(int.parse예시강제숫자형으로 변환)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-    int a = 100;
-    string b;
+int a;
+string b="100";
+
     
 
 
@@ -12,9 +13,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         void Start()
     {
-        b= a; //원래는 이렇게하면 오류가남
-        b = a.ToString(); //이렇게 하면 강제로 a를 문자열로 바꿔서 b에 넣어줌
-        // int a = "100"; 이런느낌임
+    a=b; // 당연히 숫자에다가 문자형식을 넣었으니 오류가남 이것도 강제로바꾸는방법이있음
+    a=int.Parse(b); //이렇게하면 b의 문자형식이 숫자형식으로 바뀌어서 a에 들어감
+    print(a);
+
+
+
 
 
 
