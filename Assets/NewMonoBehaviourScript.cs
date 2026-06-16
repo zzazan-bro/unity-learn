266cad263ff98c360906ce43f76aef245a8330a9
zzazan-bro <qkfrks7@gmail.com>
2026-06-16 21:44:55 +0900

    6월16일 오후 9시44분 컬렉션(arraylist,list,hashtable)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,33 +1,59 @@
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-   //1차원 배열
-int[]exp = { 50, 100, 150, 200, 250};
+    //배열
+    int[] exp = new int[5]{1,2,3,4,5};
 
-int[] array = new int[10];
+    //arraylist 자료형(실수,정수,텍스트)를 지정하지않아도 작동하지만, 연산량이 높음
 
-// 2차원 배열 -> 1차원배열이 2개있는것
-int[ , ] array2 = {{1,2,3,4,5},{10,20,30,40,50}};
+    ArrayList arrayList = new ArrayList();
 
-// 3차원 배열
+    //List 자료형을 지정해야하지만, 연산량이 줄어들어서 효율적임
 
-int[ , , ] array3 ={ {{1,2,3,4,5,} , {10,20,30,40,50}} , {{1,2,3,4,5,} , {10,20,30,40,50}} };
+    List<int> list = new List<int>();
 
+    // HashTable 인덱스(0~??까지의 순번)으로 값을 찾는게 아니라 키값으로 찾음(앞에적어준 키값을 프린트해야 뒤에있는 값이 출력됨)
+
+    Hashtable hashtable = new Hashtable();
 
 
         void Start()
     {
-print(array2[1,3]); // 결과  40
-   
-print(array2[0,1]); // 결과  2
+        arrayList.Add(1);
+        arrayList.Add(2);
+        arrayList.Add(3);
+        arrayList.Add("가나다라");
+        arrayList.Add(4);
+
+        arrayList.Remove("가나다라"); // 가나다라 를 지우겠다
+        arrayList.RemoveAt(3); // 4번째 데이터를 지우겠다
+        arrayList.RemoveRange(1,3); // 2번째부터(0이 첫번째이므로,,) 3개를 지우겠다 -> 결과 1이랑 4만 출력됨
 
 
-    print(array3[1,1,2]); // 결과 30
-    print(array3[0,0,3]); // 결과 4
+        for (int i=0; i < arrayList.Count; i++)
+        {
+            print(arrayList[i]);
+
+        }
+
+        list.Add(3); // 가능
+        list.Add("가나다"); // 불가능
+
+
+        hashtable.Add("만" , 10000);
+        hashtable.Add("백만" , 1000000);
+        hashtable.Add(50,"1억");
+
+        print(hashtable["백만"]); // 결과 1000000
+         print(hashtable["50"]); // 결과 1억
+
+        
     }
 
     // Update is called once per frame
