24fbd996da5fb870855063ac290b73079c026d26
zzazan-bro <qkfrks7@gmail.com>
2026-06-17 23:20:51 +0900

    6.17오후11시20분(프로퍼티활용 심화)은닉변수,실수방지

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -4,67 +4,63 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-
-public class Student : Human // 스튜던트라는 이름의 c#스크립트 만든 상황 ->뒤에 스크립트모노 스크립트cs가아니라 Human으로해서 상속받음
-{
-    
-string schoolName;
-
-void Start()
-    {
-        schoolName = "케이디 초등학교";
-        humanName = "케이디";
-        humanAge = 8;
-
-    info(); // 나는 인간입니다라고 휴먼클래스에서 입력되어있지만 내용을 바꾸고싶음! 그런데 부모클래스안가고 굳이 자식클래스에서 바꾸고싶을때 쓰는게 virtual임
-    }
-
-
-protected override void info()
-    {
-        base.info();  // base가 가르키는건 부모클래스를 말하는 것 -> 나는 인간입니다. 출력
-        print("나는 학생입니다."); // 나는 학생입니다는 자식클래스에서는 한 번더 출력 -> Virtual -가상함수 , Override - 재정의
-    }
-protected override void Name() // 안만들면 모클래스에서 abstract 만들어준 것 덕에 오류가 나옴 이때 override name 까지만 치면 자동완성됨
-    {
-        print(humanName);
-    }
-
-
-}
-
-
-
-
-
-
-abstract public class Human : MonoBehaviour // 휴먼이라는 이름의 c#스크립트 만든 상황 -> 아래 스트링, 인트 변수 두개만들고 save
-{
-   protected string humanName; // 처음에 스트링앞에 아무것안쓰면 타클래스에서 상속해도 사용불가능(private가 기본값이므로) 
-    // 이때 앞에 protected 를 적으면 해결됨 -> 퍼블릭은전체, 프로텍티드는 상속받은 클래스내에서만 공유가능  
-   protected int humanAge;
-
-   protected virtual void  info() // 나는 인간입니다라고 휴먼클래스에서 입력되어있지만 내용을 바꾸고싶음! 
-   // 그런데 부모클래스안가고 굳이 자식클래스에서 바꾸고싶을때 쓰는게 virtual임 -> 가상함수로 만들겠다는 뜻 이상태에서 자식클래스가서 override(재정의)하면됨
-    {
-        
-        print("나는 인간입니다.");
-    }
-abstract protected void Name(); // 자식 클래스를 만들다보면 너무많은 변수가있을 시 당연히 들어가야할 이름같은 정보를 입력안할수도있음 
-//그래서 추상함수(abstract : 사전적뜻 추상의)로 자식클래스에서 네임이라는 함수를 꼭만들어야한다고 명시해주는 기능임 -> 근데이걸 사용하면 클래스이름 자체에도
-//abstract를 붙여줘야함
-
-
-}
+//변수의 또다른 활용 프로퍼티
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
+
+
+    private int salary; // 중요한 샐러리(봉급,돈)같은 변수는 본스크립트에서만 수정가능하게 프라이베이트로 만듦
+    //프로퍼티 코드 참고
+
+    public int SalaryP{get {return salary;} private set{ salary = value;}}
+    //->이렇게 겟은 퍼블릭, 셋은 프라이베이트로 지정하면 아래에 코드 두줄은 삭제해도됨
+    private void SetSalary (int value)
+    {
+        salary = value;
+
+    }
+
+    public int GetSalary() // 타클래스에서 수정은 못하지만 읽어들일 수 있게 퍼블릭으로 만듦
+    {
+        
+        return salary;
+    }
+
+    //** 은닉변수 효율화 외 또 다른 프로퍼티 사용법 예시 2줄 -> 실수줄이기 그리고 자동완성
+    public int SalaryP{get {return salary;} set{if(vlaue <0) salary 10; else salary =value;}}
+
+    //샐러리 값에 실수로 음수를 넣는다면 10을 입력하라 , 그외의 경우엔 value값대로 입력하라
+
+    public int Bonus {get; set;}
+    //간단하게 get; set;으로만적어도 retun , value값 할당안해줘도 첫번째줄처럼 자동완성됨 get { return bonus; } set { bonus = value; }
+    
     
 
+//또다른 파일 프로그램이라는 c#스크립트(클래스)를 만듦  이름 프로그램
+
+public class Program : MonoBehaviour
+    {
+        Salary mySalary = new Salary(); // 타클래스 이름  , 변수이름 = 실체만들기 클래스이름
+
+
+
+    }
+
+    
+void Start()
+{
+    
+print(mySalary.GetSalary()); // 읽는건 가능
+
+mySalary.SetSalary(55); // 프라이베이트 로 만들어진 변수라 입력은 불가능하다고 오류뜸
+
+}
+
+//이런 이유로 은닉성유지하는 코드때문에 수정은 프라이베이트로 한줄함수 , 읽기는 퍼블릭으로 한줄함수 총 함수를 두줄써야하는데
+//은닉성 변수가 많을 수록 비효율적으로 꼭 데이터가 두줄씩 작성되어야함 이를 해결하기위한 것이 "프로퍼티임" -> 맨위로가서 프로퍼티 코드참고
 
 
 
 }
-
-    
-
