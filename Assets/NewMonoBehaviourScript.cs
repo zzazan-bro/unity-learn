85dcaff2bc96a5e53cbeaaaafa59493763485508
zzazan-bro <qkfrks7@gmail.com>
2026-06-17 22:25:51 +0900

    6.17오후10시25분(상속,protected,virtual,override등)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,75 +3,68 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Test2 : MonoBehaviour // 이건 원래 c# 스크립트 하나더만든건데 귀찮아서 여기에 클레스 파일명tes2더만듦 다파일에서 만든 함수도 event 로만든 chain에
-//넣을 수 있다는 걸 보여주기위함
-{
-    void Start()
-    {
-        Test.Onstart +=Abc; // Test에있는 Onstart라는 변수에 Abc함수 추가하겠다는 뜻 -> 스테틱 이벤트로만든 변수에서는 타 클래스에서 만든 함수를 첨부가능
-    }
-public void Abc(int value)
+
+
+public class Student : Human // 스튜던트라는 이름의 c#스크립트 만든 상황 ->뒤에 스크립트모노 스크립트cs가아니라 Human으로해서 상속받음
 {
     
-    print(value + "값이 증가했습니다");
-
-}
-}
-public class NewMonoBehaviourScript : MonoBehaviour 
-{
-    public static event ChainFucntion Onstart; // 이거는 타클래스 (다른 test2라는 c#스크립트를 만들고 거기에있는 함수도불러올수있음)
-    public delegate void ChainFunction(int value); // 델리게이트 는 클래스랑비슷한 기능, 체인펑션이라는 이름의 클래스생성 
-    // -> 함수여러개 넣어주는 공간임(hp,민첩등 다른변수도 chain한번에관리가능)
-    ChainFunction chain; // 체인펑션에 체인이라는 이름의 변수 할당
-
-    
-
-    int power;
-   int defence;
-
-   public void SetPower(int value)
-    {
-        power += value;
-        print("power의 값이" +value+ "만큼 증가했습니다 . 총 power의 값 =" + power);
-    }
-   public void SetDefence(int value)
-    {
-        defence += value;
-        print("defence의 값이" +value+ "만큼 증가했습니다 . 총 defence의 값 =" + defence);
-    }
-
-    void Start()
-    {
-      
-
-chain += SetPower; //- 체인이라는 이름변수에 셋파워 함수추가
-chain += SetDefence; //- 체인이라는 이름변수에 셋디펜스 함수 추가
-
-chain-= SetDefence; // - 체인이라는 이름변수에 셋디펜스 함수 제거 -> 이러면 파워값만 프린트됨
-    if(chain != null) // - 체인에 어떠한 함수도 없지않을경우(값이있을경우에만) 실행 -> 오류방지 조건문임
-chain(5); // - 함수에 5넣어서 실행
-
-
-
-//여기부터는 onstart변수 예시 //
+string schoolName;
 
 void Start()
-        {
-            Onstart +=SetPower;
-            Onstart +=SetDefence;
-            
-        }
-
-    }
-
-   
-    private void OnDisabel() // 게임이 꺼지면 온스타트에 5넣기
     {
-        OnStart(5);
+        schoolName = "케이디 초등학교";
+        humanName = "케이디";
+        humanAge = 8;
+
+    info(); // 나는 인간입니다라고 휴먼클래스에서 입력되어있지만 내용을 바꾸고싶음! 그런데 부모클래스안가고 굳이 자식클래스에서 바꾸고싶을때 쓰는게 virtual임
     }
 
-    void Update()
+
+protected override void info()
+    {
+        base.info();  // base가 가르키는건 부모클래스를 말하는 것 -> 나는 인간입니다. 출력
+        print("나는 학생입니다."); // 나는 학생입니다는 자식클래스에서는 한 번더 출력 -> Virtual -가상함수 , Override - 재정의
+    }
+protected override void Name() // 안만들면 모클래스에서 abstract 만들어준 것 덕에 오류가 나옴 이때 override name 까지만 치면 자동완성됨
+    {
+        print(humanName);
+    }
+
+
+}
+
+
+
+
+
+
+abstract public class Human : MonoBehaviour // 휴먼이라는 이름의 c#스크립트 만든 상황 -> 아래 스트링, 인트 변수 두개만들고 save
+{
+   protected string humanName; // 처음에 스트링앞에 아무것안쓰면 타클래스에서 상속해도 사용불가능(private가 기본값이므로) 
+    // 이때 앞에 protected 를 적으면 해결됨 -> 퍼블릭은전체, 프로텍티드는 상속받은 클래스내에서만 공유가능  
+   protected int humanAge;
+
+   protected virtual void  info() // 나는 인간입니다라고 휴먼클래스에서 입력되어있지만 내용을 바꾸고싶음! 
+   // 그런데 부모클래스안가고 굳이 자식클래스에서 바꾸고싶을때 쓰는게 virtual임 -> 가상함수로 만들겠다는 뜻 이상태에서 자식클래스가서 override(재정의)하면됨
     {
         
+        print("나는 인간입니다.");
     }
+abstract protected void Name(); // 자식 클래스를 만들다보면 너무많은 변수가있을 시 당연히 들어가야할 이름같은 정보를 입력안할수도있음 
+//그래서 추상함수(abstract : 사전적뜻 추상의)로 자식클래스에서 네임이라는 함수를 꼭만들어야한다고 명시해주는 기능임 -> 근데이걸 사용하면 클래스이름 자체에도
+//abstract를 붙여줘야함
+
+
 }
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+
+
+
+
+}
+
+    
+
