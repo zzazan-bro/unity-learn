740a4987d7caccb2f162d6d371d708eac775b907
zzazan-bro <qkfrks7@gmail.com>
2026-06-17 15:26:03 +0900

    6.17수오후3시25분 반복문(do while, foreach)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -6,44 +6,47 @@ using System.Collections.Generic;
 
 public class NewMonoBehaviourScript : MonoBehaviour 
 {
+// for문 반복횟수가 명확할때 vs while 반복횟수가 명확하지않을때
 
-    int num = 0; // -> 1항 규칙 대채식 표기값 이미 할당했음 넘은 0으로
+// do while -> while은 와일 괄호()안에 조건을 먼저 생각하고 실행함 but, do while은 do먼저 실행하고 그담에 와일()괄호 안의 조건을 생각함 
+// 만약 조건이맞지않는다면 1회차만 Do를 실행하고 그다음부터는 실행하지않음
+
+// foreach는 긴 텍스트 값을 텍스트안의 글자여러개를 글자하나로 쪼개겠다는 뜻임
+    int num = 0;
+
+    string text=("가나다라마바사"); 
+
+
+
         void Start()
     {
-        for(int i = 0; i <10; i++)
+       
+           while(num < 10)
         {
-             num =i;
+            num++;
+
             print(num);
-           
+
         }
 
-      //  print(num++); -> for(타입선언 변수이름 = 0-> 0부터 ; i<10 -> i가 10보다 작을때까지반복함 i++->자신에게 계속 1을 더함 * 1항과 2항 3항은 필수아님)
-        //print(num++); 이렇게 10번 반복할거를 그냥 for반복문 한줄로 끝낼수있음
-       // print(num++); 필수아닐때 작성법
-       // for( ; ; )
-       //{ if(num>=10)
-       //   break;-> 위 두줄은 2항 규칙 대채식 넘이 10보다같거나 크면 반복문or조건문 빠져나가기
-       //print(num;)
-       //   num +=2; -> 3항 규칙대체식}
-       // print(num++);
+        do
+        {
+            
+            num++;
+            print(num);
+            
+        }
+        while(num<10);
+
+   foreach(char a in text) // text라는 긴변수를 쪼개서 a라는 변수에 하나씩 char타입으로 담겠다는 뜻
+   {
+print(a);
+    
+   }
 
     }
-    void Strat()
-    {
-        
-        for( ; ; )
+   
 
-        num++
-
-        if(num % 2 ==0)
-        continue; // 해당 회차를 끝냄.
-
-        pirnt(num);
-
-        if (num >10)
-        break; // 첫번째 반복문 또는 조건문 탈출
-
-    }
 
     // Update is called once per frame
     void Update()
