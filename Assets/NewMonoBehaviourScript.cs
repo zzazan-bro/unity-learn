fc43ead5d466ea227bee97d11a857a68d5180ad5
zzazan-bro <qkfrks7@gmail.com>
2026-06-17 18:51:48 +0900

    6.17수6시51분 구조체(struct설명)

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -4,45 +4,34 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+public struct Youtube //구버전 a값을 직접 지정할수없어 스트럭트로 설계하고 클래스에서 함수식이나 a를직접불러와서 값을 넣어줘야함
+{
+    public int a;
+
+
+public void GetA(int value)
+    {
+        a = value;
+    }
+
+}
+
 public class NewMonoBehaviourScript : MonoBehaviour 
 {
-// for문 반복횟수가 명확할때 vs while 반복횟수가 명확하지않을때
 
-// do while -> while은 와일 괄호()안에 조건을 먼저 생각하고 실행함 but, do while은 do먼저 실행하고 그담에 와일()괄호 안의 조건을 생각함 
-// 만약 조건이맞지않는다면 1회차만 Do를 실행하고 그다음부터는 실행하지않음
-
-// foreach는 긴 텍스트 값을 텍스트안의 글자여러개를 글자하나로 쪼개겠다는 뜻임
-    int num = 0;
-
-    string text=("가나다라마바사"); 
-
+Youtube keidy; //유튜브 스트럭트 설계도를 불러와서 keidy라는 이름의 변수상자를 만듦(뒤에 =new Youtube(); 이거는 스트럭트에서 가져온거라 알아서실행됨, 만약 class였다면
+//적어줘야함!
 
 
         void Start()
-    {
-       
-           while(num < 10)
-        {
-            num++;
+    { keidy.a=5;
+    keidy.GetA(5);
 
-            print(num);
-
-        }
-
-        do
-        {
-            
-            num++;
-            print(num);
-            
-        }
-        while(num<10);
-
-   foreach(char a in text) // text라는 긴변수를 쪼개서 a라는 변수에 하나씩 char타입으로 담겠다는 뜻
-   {
-print(a);
     
-   }
+       
+      
+
+
 
     }
    
